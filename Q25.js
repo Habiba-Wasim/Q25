5365e57c7cdf253d9fa862d01d16b39446bd796d
Habiba <160347457+Habiba-Wasim@users.noreply.github.com>
2024-03-17 22:39:14 +0500

    Step 25 of Typescript
    Step 25 of Typescript

---
+++ Q25.js
@@ -0,0 +1,15 @@
+"use strict";
+// Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+// • Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+// • Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+let alienColor = "green";
+// if (alienColor === "green"){
+//      console.log("Player just earned 5 points!")
+// }
+//alienColor = "red";
+if (alienColor === "green") {
+    console.log("Player just earned 5 points!");
+}
+else {
+    console.log("My favourite color is black");
+}
